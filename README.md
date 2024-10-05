f36602aae2d902916c7177436e832f4fb2355d61
Drew <91377105+Drewski6@users.noreply.github.com>
2024-10-05 17:57:17 +0200

    Made bind mount to django project files. Including blank django files along with new app called api_app

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Dockerized SPA using Django and Vanilla JS
 
 - This project is a sample of how to create a dockerized single page application (SPA) using django as a backend and vanilla js as the frontend.
+
+- .env
+  - Need to creat a .env file at the root for configuration.
+  - Need:
+    - SITE_NAME=""
+      - Indicate the name of your site/django project name.
+    - APP_NAME=""
+      - Indicate the name of the app we are making.
--- django/tools/docker-entrypoint.sh
+++ django/tools/docker-entrypoint.sh
@@ -2,11 +2,20 @@
 
 set -ux
 
-echo "Starting django setup"
+echo "Starting django setup script"
 
-# check that the project does not exist, if it does not exist, create it
+cd /app/django_data
+
+# Check that the project does not exist, if it does not exist, create it
 if [ ! -d "./${SITE_NAME}" ]; then
+  echo "Creating new django project"
   python3 -m django startproject $SITE_NAME
+  cd $SITE_NAME
+  python3 manage.py startapp $APP_NAME
+else
+  echo "Django project already exists"
 fi
+
+echo "Starting django server"
 
 tail -f /dev/null
